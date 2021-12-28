dfdd628d373e4c3bbcd8a8f125f70fc5a33e2456
Ngoc Truong <ngoc.truong@posteo.de>
2021-12-28 22:24:21 +0100

    uibuild

--- index.js
+++ index.js
@@ -9,6 +9,7 @@ morgan.token("body", (request) => {
 
 app.use(cors());
 app.use(express.json());
+app.use(express.static("build"));
 app.use(
   morgan(":method :url :status :res[content-length] :response-time ms :body")
 );
